5c507a6d1b877996c530d5f3b03b104c4878a16d
CodySt0 <Codistgeorge@gmail.com>
2024-06-13 10:14:44 -0700

    added to do to do list

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const button = document.getElementById("add-todo");
 console.log(button);
 //Function to run when the button is clicked
 function handleAddTodo() {
-  console.log("button is clicked!");
+  const ul = document.querySelector("ul"); //acessing and element
+  console.log(ul);
+  //create an li to go into my unordered list
+  const li = document.createElement("li"); //creating a new element
+  console.log("created list item", li);
+  //access the input element
+  const input = document.querySelector("input");
+  console.log(input.value);
+  //set the input text to my list item text
+  li.textContent = input.value;
+  console.log(li);
+  //append(add) my list item to my unordered lists
+  //( as a child element)
+  ul.appendChild(li);
 }
 // add an event listener to my button
-button.addEventListener("click", handleAddToDo);
+button.addEventListener("click", handleAddTodo);
